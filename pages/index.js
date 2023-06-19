36c1fa6740073301a19efdfd27469cc82227b497
BXBZwe <Legendaryzwe@gmail.com>
2023-06-19 17:37:17 +0700

    4th commit

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,20 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-const SignUpPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+import reportWebVitals from './reportWebVitals';
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your sign up logic here
-  };
+import ADMINING from './userpage/home';
 
-  const containerStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundImage: "url(https://picsum.photos/id/42/600/400)",
-    backgroundSize: 'cover',
-  };
 
-  const loginBoxStyle = {
-    
-    backgroundColor: '#f0f0f0',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '45px',
-    borderRadius: '16px',
-  };
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ADMINING />
+  </React.StrictMode>
+);
 
-  const buttonStyle = {
-    backgroundColor: '#BBE4BA',
-    marginBottom: '0.5rem',
-    width: '55%',
-    margin: '5px 22% auto',
-  };
-
-  return (
-    <div style={containerStyle}>
-      <div style={loginBoxStyle}>
-        <h1 style={{textAlign: 'center', marginBottom: '25px', fontFamily: 'cursive'}}>MyPantry</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div  className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <div style={{marginBottom: '20px'}} className="form-group">
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-
-          <button type="submit" className="btn my-auto" style={buttonStyle}>
-            Sign Up
-          </button>
-          <button className="btn" style={buttonStyle}>
-            Cancel
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
-
-export default SignUpPage;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
